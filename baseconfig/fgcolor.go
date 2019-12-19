41d59461cca360e9d3a0634fa6545bea9219eed6
Pezhman Kasraee <pezhmankasraee@gmail.com>
2019-12-19 13:57:22 +0100

    add fg colors for different level of logs
    Tracking-id: US003:T001

---
+++ baseconfig/fgcolor.go
@@ -0,0 +1,31 @@
+/**
+Copyright 2019 Pezhman Kasraee
+
+This file is part of go-pklog.
+
+go-pklog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+go-pklog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package baseconfig
+
+// List of all foreground colors
+const (
+	FGBlack   = "\033[1;30m"
+	FGRed     = "\033[1;31m"
+	FGGreen   = "\033[1;32m"
+	FGYellow  = "\033[1;33m"
+	FGPurple  = "\033[1;34m"
+	FGMagneta = "\033[1;35m"
+	FGTeal    = "\033[1;36m"
+	FGWhite   = "\033[1;37m"
+	Reset     = "\033[0m"
+)
